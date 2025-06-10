d487855a84385b9b1403adcf98f00a2120acf866
idibs <tiagosimoesprado@gmail.com>
2025-06-10 10:48:45 -0300

    Arrumando tabela de estoque e funcionario

--- src/components/Tabela.tsx
+++ src/components/Tabela.tsx
@@ -1,4 +1,8 @@
 import { useState } from 'react';
+import { MdEdit } from "react-icons/md";
+import { FaTrash } from "react-icons/fa";
+import { Link } from "react-router-dom"
+
 
 export default function Tabela({ inputs, data }) {
     const [filters, setFilters] = useState({
@@ -28,9 +32,9 @@ export default function Tabela({ inputs, data }) {
 
     return (
         <div className='w-full max-w-6xl mx-auto p-4'>
-            <div className="grid grid-cols-2 gap-4 mb-1">
                 <div>
                     <h1 className="font-bold">Filtros:</h1>
+                    <div className='flex justify-between'>
                     <div className="grid grid-cols-2 py-2 gap-2">
                         {inputs.map((element, idx) => (
                             <input
@@ -44,14 +48,11 @@ export default function Tabela({ inputs, data }) {
                             />
                         ))}
                     </div>
+                    <button type="button" className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                        Criar registro
+                        </button>
+                        </div>
                 </div>
-                <div>
-                    <h1 className="font-bold">clp</h1>
-                    <div className="grid grid-cols-2 py-2 gap-2">
-                        <button className=''></button>
-                    </div>
-                </div>
-            </div>
             <div>
                 <table className="table-auto border-collapse w-full">
                     <thead>
@@ -59,20 +60,25 @@ export default function Tabela({ inputs, data }) {
                             {Object.keys(data[0]).map((key) => (
                                 <th key={key} className="border border-gray-500 px-1 py-1">{key}</th>
                             ))}
-                            <th className="border border-gray-500 px-1 py-1">Opções</th>
+                                <th className="border border-gray-500 px-1 py-1">Opções</th>
+                            
                         </tr>
                     </thead>
                     <tbody>
                         {filteredData.map((item, index) => (
-                            <tr key={index}>
-                                {Object.values(item).map((value, idx) => (
-                                    <td key={idx} className="text-center border border-gray-500 py-1">{value}</td>
-                                ))}
-                                <td className="text-center border border-gray-500 py-1">
-                                    <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Editar</button>
-                                    <button>Excluir</button>
-                                </td>
-                            </tr>
+                                <tr key={index} className='hover:bg-[#EDECEC]'>
+                                    {Object.values(item).map((value, idx) => (
+                                        <td key={idx} className="text-center border border-gray-500 py-1">{value}</td>
+                                    ))}    
+                                    <td className="text-center border flex justify-evenly py-1"> 
+                                        <Link to="/estoque">
+                                            <MdEdit />
+                                        </Link>
+                                        <a>
+                                            <FaTrash />
+                                        </a>
+                                    </td>
+                                </tr>
                         ))}
                     </tbody>
                 </table>
